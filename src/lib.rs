5f1c5eb3f443ce8205cb83684c483d89d3d7f568
Christopher J. Covert <covertops5@gmail.com>
2022-09-24 17:13:59 -0400

    feat: remove wee_alloc

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,6 @@ use std::fmt::Display;
 
 use wasm_bindgen::prelude::*;
 
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
 pub fn initialize() {
     wasm_logger::init(wasm_logger::Config::default());
